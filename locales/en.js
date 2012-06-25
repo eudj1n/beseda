36059bee1aaaa637921541374f1c31a0018b5631
eudj1n <eudjin@gmail.com>
2012-06-25 15:00:24 +0600

    True initial commit

---
+++ locales/en.js
@@ -0,0 +1,17 @@
+{
+	"name": "Anonymous chat Virt.IN",
+	"base": "http://virtin.local",
+	"connecting": "Connection to server...",
+	"your_sex": "Your sex",
+	"your_age": "Your age",
+	"com_sex": "Companion sex",
+	"com_age": "Companion age",
+	"male": "Male",
+	"female": "Female",
+	"start": "Start chat",
+	"online": "Online",
+	"send": "Send",
+	"disconnect": "Disconnect",
+	"share": "Share with friends:",
+	"disconnected": "disconnected"
+}
